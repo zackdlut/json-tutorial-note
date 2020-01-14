329cdb4ac8098fbb339efd6713db0b8a5e258f4a
root <root@localhost.localdomain>
2020-01-13 21:22:45 -0500

    json-note

---
+++ tutorial01/leptjson.h
@@ -0,0 +1,24 @@
+#ifndef _LEPTJSON_
+#define _LEPTJSON_
+
+
+typedef enum {LEPT_NULL,LEPT_FALSE,LEPT_TRUE,LEPT_NUMBER,LEPT_STRING,LEPT_ARRaY,LEPT_OBJECT} lept_type;
+typedef struct {
+    double value;
+    lept_type type;
+} lept_value;
+
+
+enum {
+    LEPT_PARSE_OK = 0,
+    LEPT_PARSE_EXPECT_VALUE,
+    LEPT_PARSE_INVALID_VALUE,
+    LEPT_PARSE_ROOT_NOT_SINGULAR,
+    LEPT_PARSE_NUMBER_TOO_BIG
+};
+
+int lept_parse(lept_value* v, const char* json);
+lept_type lept_get_type(const lept_value* v);
+double lept_get_number(const lept_value*v);
+
+#endif //_LEPTJSON_
---
+++ tutorial01/test.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "leptjson.h"
+
+
+static int main_ret = 0;
+static int test_count = 0;
+static int test_pass = 0;
+
+#define EXPECT_EQ_BASE(equality,expect,actual, format) \
+    do {\
+        test_count++;\
+        if (equality) \
+            test_pass++;\
+        else {\
+            fprintf(stderr,"%s:%d: expect :" format " actual: " format "\n", __FILE__,__LINE__,expect,actual);\
+            main_ret = 1;\
+        }\
+    }while(0)
+
+#define EXPECT_EQ_INT(expect,actual) EXPECT_EQ_BASE((expect) == (actual),expect,actual,"%d")
+#define EXPECT_EQ_DOUBLE(expect,actual) EXPECT_EQ_BASE((expect) == (actual),expect,actual,"%.17g")
+
+static void test_parse_null(){
+    lept_value v;
+    v.type = LEPT_TRUE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK,lept_parse(&v,"null"));
+    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+}
+
+static void test_parse_expect_value(){
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE,lept_parse(&v,""));
+    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE,lept_parse(&v," "));
+    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+}
+
+static void test_parse_invalid_value(){
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE,lept_parse(&v,"nu"));
+    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE,lept_parse(&v,"?"));
+    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+}
+
+static void test_parse_root_not_singular(){
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR,lept_parse(&v,"null x"));
+    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+
+}
+
+static void test_parse_true(){
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK,lept_parse(&v,"true"));
+    EXPECT_EQ_INT(LEPT_TRUE,lept_get_type(&v));
+
+}
+
+static void test_parse_false(){
+    lept_value v;
+    v.type = LEPT_TRUE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK,lept_parse(&v,"false"));
+    EXPECT_EQ_INT(LEPT_FALSE,lept_get_type(&v));
+
+}
+
+#define TEST_NUMBER(expect,json) \
+    do {\
+        lept_value v;\
+        EXPECT_EQ_INT(LEPT_PARSE_OK,lept_parse(&v,json));\
+        EXPECT_EQ_INT(LEPT_NUMBER,lept_get_type(&v));\
+        EXPECT_EQ_DOUBLE(expect,lept_get_number(&v));\
+    }while(0)
+
+static void test_parse_number(){
+    TEST_NUMBER(0.0,"0");
+    TEST_NUMBER(0.0,"-0");
+    TEST_NUMBER(0.0,"-0.0");
+    TEST_NUMBER(1.0,"1.0");
+    TEST_NUMBER(1.0,"1");
+    TEST_NUMBER(1.0,"-1");
+}
+
+static void test_parse(){
+    test_parse_null();
+    test_parse_expect_value();
+    test_parse_invalid_value();
+    test_parse_root_not_singular();
+
+    test_parse_true();
+    test_parse_false();
+
+    test_parse_number();
+
+}
+
+int main(int argc, char const *argv[])
+{
+    test_parse();
+    printf("%d/%d (%3.2f%%) passed\n",test_pass,test_count,test_pass*100.0/test_count);
+    return 0;
+}
